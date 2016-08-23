799f8e10b353373eb93bf988974cec256746b4c2
Melissa Rodriguez <olenellina@gmail.com>
2016-08-23 11:29:46 -0700

    doing a commit now with working functionality, but added skeleton for optional owners class

---
+++ bank_account.rb
@@ -0,0 +1,60 @@
+# Optional:
+#
+# Create an Owner class which will store information about those who own the Accounts.
+#   This should have info like name and address and any other identifying information that an account owner would have.
+# Add an owner property to each Account to track information about who owns the account.
+#   The Account can be created with an owner, OR you can create a method that will add the owner after the Account has already been created.
+
+
+module Bank
+  class Account
+    attr_reader :balance
+    def initialize(id, balance)
+      unless balance >= 0
+        raise ArgumentError.new("A new account cannot be created with initial negative balance")
+      end
+      @id = id
+      @balance = balance
+    end
+
+    def withdraw(withdrawAmount)
+      newBalance = @balance - withdrawAmount
+      if newBalance < 0
+        puts "Sorry, you cannot have a negative balance"
+        return @balance
+      else
+        @balance -= withdrawAmount
+        return @balance
+      end
+    end
+
+    def deposit(depositAmount)
+      @balance += depositAmount
+      return @balance
+    end
+  end
+
+  class owner
+    def initialize(id, name, address)
+      @AccountId = id
+      @name = name
+      @address = address
+    end
+
+    def printInfo(id)
+      puts "#{id}'s name is #{name}'"
+    end
+  end
+end
+
+
+myAccount = Bank::Account.new(123, 40)
+# myAccount = Bank::Account.new(123, -40)
+
+
+myAccount.withdraw(20)
+puts myAccount.balance
+myAccount.deposit(20)
+puts myAccount.balance
+myAccount.withdraw(60)
+puts myAccount.balance
