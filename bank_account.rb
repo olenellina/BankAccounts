ed58eda9ba27a098f2c60e4e71d616704b230e2a
Melissa Rodriguez <olenellina@gmail.com>
2016-08-23 13:59:34 -0700

    committing changes to the bank account program. This is the final working version with all enchancements. Just needs comments.

--- bank_account.rb
+++ bank_account.rb
@@ -6,47 +6,50 @@ module Bank
         raise ArgumentError.new("A new account cannot be created with initial negative balance")
       end
       @id = id
-      @balance = sprintf('%0.2f', balance.round(2))
+      @balance = balance.to_f
+      puts "Account ID #{id} has been created!"
       @owner = Owner.new(id, name, address)
     end
 
     def withdraw(withdrawAmount)
       newBalance = @balance - withdrawAmount
       if newBalance < 0
-        puts "Sorry, you cannot have a negative balance"
+        puts "Sorry, you cannot have a negative balance. The most you can withdraw is $#{nicePrint(@balance)}."
         return @balance
       else
         @balance -= withdrawAmount
+        puts "You have made a withdraw of $#{withdrawAmount.to_f}. Your remaining balance is $#{nicePrint(@balance)}."
         return @balance
       end
     end
 
     def deposit(depositAmount)
       @balance += depositAmount
+      puts "You have made a deposit of $#{depositAmount.to_f}. Your new balance is $#{nicePrint(@balance)}."
       return @balance
+    end
+
+    # the NicePrint method will return balance formatted as money.
+    def nicePrint(balance)
+      return sprintf('%0.2f', balance.round(2))
     end
   end
 
   class Owner
-
     attr_reader :name, :address
     def initialize(id, name, address)
       @name = name
       @address = address
-      puts "The owner of account ID #{id} is #{@name}"
+      puts "The owner of Account ID #{id} is #{@name}"
     end
   end
 end
 
-
 # myAccount = Bank::Account.new(123, 40)
 # myAccount = Bank::Account.new(123, -40)
-myAccount = Bank::Account.new(123, "Mel", "Seattle", 40)
+myAccount = Bank::Account.new(123, "Melissa Rodriguez", "Kirkland, WA", 40)
 
 
 myAccount.withdraw(20)
-puts myAccount.balance
 myAccount.deposit(20)
-puts myAccount.balance
 myAccount.withdraw(60)
-puts myAccount.balance
